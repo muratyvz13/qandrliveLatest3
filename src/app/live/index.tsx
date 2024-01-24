e785e1076be6b539aa0f55d50c00dfb1006131db
Murat Yavuz <muratyavuz@192.168.1.76>
2024-01-24 17:02:47 +0300

    a

--- src/app/live/index.tsx
+++ src/app/live/index.tsx
@@ -20,7 +20,7 @@ export const Live: React.FunctionComponent<LiveProps> = ({ }) => {
   const [walletAddress,setWalletAddress]=useState("");
   const [username, setUsername] = useState("");
   const [question, setQuestion] = useState("");
-  const [quizName, setQuizName] = useState("Digiverse Live Quiz");
+  const [quizName, setQuizName] = useState("GT Protocol Live Quiz");
   const [options, setOptions] = useState(["1", "2", "3", "4"]);
   const [trueAnswer, setTrueAnswer] = useState("");
   const [userAnswer, setUserAnswer] = useState(""); 
