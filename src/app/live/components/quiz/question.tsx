ad3cb06733a74fe313942851dc3c77b9099d8ab7
Murat Yavuz <muratyvz13@gmail.com>
2024-02-23 17:22:13 +0300

    make it better

--- src/app/live/components/quiz/question.tsx
+++ src/app/live/components/quiz/question.tsx
@@ -459,7 +459,12 @@ const QuizQuestion: React.FC<Props> = ({ onClick, countDown,waiting,currentQuest
                 }
 
                 {waitingState === 1 &&
-                  <Image src={'/img/advertalt.png'} className={"banner-image"}/>
+
+                   (currentQuestionIndex % 2 === 0 ? (
+        <Image src={'/img/advertalt.png'} className={"banner-image"} />
+      ) : (
+        <Image src={'/img/banner-image.png'} className={"banner-image"} />
+      ))
                 }
 
 {/* </SwiperSlide> */}
